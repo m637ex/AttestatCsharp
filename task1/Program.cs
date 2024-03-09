aacf6f773dcfbcea315ce57934f0bd5782443130
m637ex <m637ex@gmail.com>
2024-03-09 15:49:35 +0700

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод значений M и N
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Вызов рекурсивной функции для вывода чисел от M до N
+        PrintNumbersInRange(m, n);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу
+    }
+
+    static void PrintNumbersInRange(int start, int end)
+    {
+        // Базовый случай: если начальное значение больше конечного
+        if (start > end)
+            return;
+
+        // Вывод текущего значения
+        Console.WriteLine(start);
+
+        // Рекурсивный вызов для следующего числа
+        PrintNumbersInRange(start + 1, end);
+    }
+}
