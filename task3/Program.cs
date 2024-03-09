38c3f664e8926da5da3231d00ace8aa5db31f1ca
m637ex <m637ex@gmail.com>
2024-03-09 15:47:28 +0700

    atestat c#

---
+++ task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Произвольный массив чисел
+        int[] array = { 1, 2, 3, 4, 5 };
+
+        // Вызов рекурсивной функции для вывода элементов массива
+        PrintArrayReverse(array, array.Length - 1);
+
+        Console.ReadLine(); // Чтобы консольное окно не закрывалось сразу
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        // Базовый случай: если индекс стал меньше 0 (выход за границы массива)
+        if (index < 0)
+            return;
+
+        // Вывод текущего элемента массива
+        Console.WriteLine(array[index]);
+
+        // Рекурсивный вызов для следующего элемента массива (с предыдущим индексом)
+        PrintArrayReverse(array, index - 1);
+    }
+}
